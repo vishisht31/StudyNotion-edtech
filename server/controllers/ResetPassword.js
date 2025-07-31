817af22f1f9cefabe5c936795f8aea1e2ea76502
vishisht31 <vishishtmaroria31@gmail.com>
2025-07-31 11:28:25 +0530

    minor changes

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studynotion-edtech-mygq.onrender.com/update-password/${token}`;
 
 		await mailSender(
 			email,
